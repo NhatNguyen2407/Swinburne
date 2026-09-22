3aeec1f7e6d1dcdab725639bed08600e39360d1f
Nguyen Huyen Minh Nhat <john.bibibi@gmail.com>
2026-09-22 11:01:42 +0700

    Add application and MSIX source configuration

---
+++ SWE40006_Software_Deployment_and_Evolution/HelloWiX/Form1.Designer.cs
@@ -0,0 +1,26 @@
+namespace HelloWiX
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+    }
+}
