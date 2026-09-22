63739d46161d9364af34f6886543e04206c466db
Nguyen Huyen Minh Nhat <john.bibibi@gmail.com>
2026-09-22 11:00:54 +0700

    Add HelloWiX Task 1 source files

---
+++ SWE40006_Software_Deployment_and_Evolution/HelloWiX.Installer/HelloWiX.Core/GreetingService.cs
@@ -0,0 +1,10 @@
+namespace HelloWiX.Core
+{
+    public static class GreetingService
+    {
+        public static string GetMessage()
+        {
+            return "Hello from HelloWiX.Core!";
+        }
+    }
+}
